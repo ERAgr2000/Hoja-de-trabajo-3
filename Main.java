58f8e4fc856f81076afc14da41afaf2c49f98f81
Elder <53918860+ERAgr2000@users.noreply.github.com>
2021-02-21 19:07:09 -0600

    radix
    Radix sort is working

--- Main.java
+++ Main.java
@@ -132,12 +132,82 @@ public class Main{
 		//Metodo Radix, es el que implementa algoritmo de ordenamiento Radix sort
 		//Parametro: lista_ordenar es una estructura de datos con implementacion de objetos con interfaz Comparable, es la lista que se ordenara
 		//Return: lista_ordenada es una lista 
-		public static < T extends Comparable > T[] radix(T[] lista_ordenar)
-		{
-			
-			
-			return lista_ordenada;
+		
+		static int getMax(int arr[], int n) 
+		{ 
+			int mx = arr[0]; 
+			for (int i = 1; i < n; i++) 
+				if (arr[i] > mx) 
+					mx = arr[i]; 
+			return mx; 
+		} 
+
+		// A function to do counting sort of arr[] according to 
+		// the digit represented by exp. 
+		static void countSort(int arr[], int n, int exp) 
+		{ 
+			int output[] = new int[n]; // output array 
+			int i; 
+			int count[] = new int[10]; 
+			Arrays.fill(count, 0); 
+
+			// Store count of occurrences in count[] 
+			for (i = 0; i < n; i++) 
+				count[(arr[i] / exp) % 10]++; 
+
+			// Change count[i] so that count[i] now contains 
+			// actual position of this digit in output[] 
+			for (i = 1; i < 10; i++) 
+				count[i] += count[i - 1]; 
+
+			// Build the output array 
+			for (i = n - 1; i >= 0; i--) { 
+				output[count[(arr[i] / exp) % 10] - 1] = arr[i]; 
+				count[(arr[i] / exp) % 10]--; 
+			} 
+
+			// Copy the output array to arr[], so that arr[] now 
+			// contains sorted numbers according to curent digit 
+			for (i = 0; i < n; i++) 
+				arr[i] = output[i]; 
+		} 
+
+		// The main function to that sorts arr[] of size n using 
+		// Radix Sort 
+		static void radixsort(int arr[], int n) 
+		{ 
+			// Find the maximum number to know number of digits 
+			int m = getMax(arr, n); 
+
+			// Do counting sort for every digit. Note that 
+			// instead of passing digit number, exp is passed. 
+			// exp is 10^i where i is current digit number 
+			for (int exp = 1; m / exp > 0; exp *= 10) 
+				countSort(arr, n, exp); 
 		}
+	
+		
+		public static int[] radix(int[] lista_ordenar){
+			
+			try {
+				
+				int n = lista_ordenar.length;
+				
+				radixsort(lista_ordenar, n);
+				print(lista_ordenar, n); 
+				return lista_ordenar;
+			}
+			catch(Exception e) {
+				return lista_ordenar;
+			}
+			
+		}
+		
+		static void print(int arr[], int n) 
+		{ 
+			for (int i = 0; i < n; i++) 
+				System.out.print(arr[i] + " "); 
+		} 
 		
 		//Metodo Bubble, es el que implementa algoritmo de ordenamiento Bubble sort
 		//Parametro: lista_ordenar es una estructura de datos con implementacion de objetos con interfaz Comparable, es la lista que se ordenara
@@ -166,8 +236,9 @@ public class Main{
 	public static void main(String args[]) 
 	{
 		Integer[] orden = {2,4,3,1};
-		Integer[] orden2 = {2,4,3,1,10,9,7,8,6};
-		System.out.print(SortM.<Integer>(orden2));
+		int[] orden2 = {2,4,3,1,10,9,7,8,6};
+		SortM.radix(orden2);
+		
 		
 	}
 

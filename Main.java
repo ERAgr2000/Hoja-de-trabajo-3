7d9f90a170cbc8da774fbcbe12a43666d0357d1f
Elder <53918860+ERAgr2000@users.noreply.github.com>
2021-02-21 19:44:07 -0600

    sorts
    All sort are working

--- Main.java
+++ Main.java
@@ -46,89 +46,153 @@ public class Main{
 			
 			return lista_ordenada;
 		}
-		/*
+		
 		//Metodo Merge, es el que implementa algoritmo de ordenamiento Merge sort
 		//Parametro: lista_ordenar es una estructura de datos con implementacion de objetos con interfaz Comparable, es la lista que se ordenara
 		//Return: lista_ordenada es una lista 
 		public static < T extends Comparable > ArrayList<T> merge(T[] lista_ordenar)
 		{
-			int i = 0;
-			int size = lista_ordenar.length;
+			int n = lista_ordenar.length;
 			
-			
-			
+			sortMerge(lista_ordenar, 0, lista_ordenar.length - 1);
+			ArrayList<T> lista_ordenada = new ArrayList<>();
+			for(int i = 0; i < n; i=i+1){
+				lista_ordenada.add(lista_ordenar[i]);
+			}
 			return lista_ordenada;
 		}
+		
+		// Merges two subarrays of arr[].
+		// First subarray is arr[l..m]
+		// Second subarray is arr[m+1..r]
+		static <T extends Comparable<T> > void merge(T[] arr, int l, int m, int r)
+		{
+			// Find sizes of two subarrays to be merged
+			int n1 = m - l + 1;
+			int n2 = r - m;
+
+			/* Create temp arrays */
+			ArrayList<T> L = new ArrayList<>();
+			ArrayList<T> R = new ArrayList<>();
+
+			/*Copy data to temp arrays*/
+			for (int i = 0; i < n1; ++i)
+				L.add(arr[l + i]);
+			for (int j = 0; j < n2; ++j)
+				R.add(arr[m + 1 + j]);
+
+			/* Merge the temp arrays */
+
+			// Initial indexes of first and second subarrays
+			int i = 0, j = 0;
+
+			// Initial index of merged subarry array
+			int k = l;
+			while (i < n1 && j < n2) {
+				if (L.get(i).compareTo(R.get(j)) <= 0) {
+					arr[k] = L.get(i);
+					i++;
+				}
+				else {
+					arr[k] = R.get(j);
+					j++;
+				}
+				k++;
+			}
+
+			/* Copy remaining elements of L[] if any */
+			while (i < n1) {
+				arr[k] = L.get(i);
+				i++;
+				k++;
+			}
+
+			/* Copy remaining elements of R[] if any */
+			while (j < n2) {
+				arr[k] = R.get(j);
+				j++;
+				k++;
+			}
+		}
+
+		// Main function that sorts arr[l..r] using
+		// merge()
+		static < T extends Comparable<T> > void sortMerge(T[] arr, int l, int r)
+		{
+			if (l < r) {
+				// Find the middle point
+				int m =l+ (r-l)/2;
+
+				// Sort first and second halves
+				sortMerge(arr, l, m);
+				sortMerge(arr, m + 1, r);
+
+				// Merge the sorted halves
+				merge(arr, l, m, r);
+			}
+		}
+
 		
 		//Metodo Quick, es el que implementa algoritmo de ordenamiento Quick sort. Se utilizara como pivote el ultimo elemento.
 		//Parametro: lista_ordenar es una estructura de datos con implementacion de objetos con interfaz Comparable, es la lista que se ordenara
 		//Return: lista_ordenada es una lista 
 		public static < T extends Comparable > ArrayList<T> quick(T[] lista_x_ordenar)
 		{
-			ArrayList<T> lista_ordenar = new ArrayList<>(Arrays.asList(lista_x_ordenar));
-			return quick(lista_ordenar);
-			
-		}
-		
-		public static < T extends Comparable > ArrayList<T> quick(ArrayList<T> lista_x_ordenar)
-		{
-			int size = lista_x_ordenar.size();
-			int i = 0;
-			
-			
-			ArrayList<T> lista_menor = new ArrayList<T>();
-			ArrayList<T> lista_mayor = new ArrayList<T>();
-			
-			T pivot = lista_x_ordenar.get(size - 1);
-		
-			while(i < size-1){
-				
-				if( pivot.compareTo(lista_x_ordenar.get(i)) < 0 ){
-					
-					lista_menor.add(lista_x_ordenar.get(i));
-					
-				}else{
-					
-					lista_mayor.add(lista_x_ordenar.get(i));
-				}
-				i++;
-			}
-		
-			int len_menor = lista_menor.size();
-			int len_mayor = lista_mayor.size();
-			
-			ArrayList<T> lista_mayor_ordenada;
-			ArrayList<T> lista_menor_ordenada;
-			
-			if(len_menor > 0){
-				lista_mayor_ordenada = quick(lista_mayor);
-			}
-			else{
-				lista_mayor_ordenada = lista_mayor;
-			}
-			if(len_mayor > 1){
-				lista_menor_ordenada = quick(lista_menor);
-			}
-			else{
-				lista_menor_ordenada = lista_menor;
-			}
-			
-			
+			int n = lista_x_ordenar.length;
+			sort(lista_x_ordenar, 0 , n-1);
 			ArrayList<T> lista_ordenada = new ArrayList<>();
-			
-			for(i = 0; i < len_menor; i = i+1){
-				lista_mayor_ordenada.add(lista_menor_ordenada.get(i));
+			for(int i = 0; i < n; i=i+1){
+				lista_ordenada.add(lista_x_ordenar[i]);
 			}
-			lista_ordenada.add(pivot);
-			for(i = 0; i < len_mayor; i = i+1){
-				lista_mayor_ordenada.add(lista_mayor_ordenada.get(i));
-			}
-			
 			return lista_ordenada;
-			
 		}
 		
-		*/
+		static  <T extends Comparable<T> > int partition(T[] arr, int low, int high) 
+		{ 
+			T pivot = arr[high]; 
+			int i = (low-1); // index of smaller element 
+			for (int j=low; j<high; j++) 
+			{ 
+				// If current element is smaller than the pivot 
+				if (arr[j].compareTo(pivot) < 0) 
+				{ 
+					i++; 
+
+					// swap arr[i] and arr[j] 
+					T temp = arr[i]; 
+					arr[i] = arr[j]; 
+					arr[j] = temp; 
+				} 
+			} 
+
+			// swap arr[i+1] and arr[high] (or pivot) 
+			T temp = arr[i+1]; 
+			arr[i+1] = arr[high]; 
+			arr[high] = temp; 
+
+			return i+1; 
+		} 
+
+
+		/* The main function that implements QuickSort() 
+		arr[] --> Array to be sorted, 
+		low --> Starting index, 
+		high --> Ending index */
+		
+		static <T extends Comparable<T> > void sort(T[] arr, int low, int high) 
+		{ 
+			if (low < high) 
+			{ 
+				
+				int pi = partition(arr, low, high); 
+
+				// Recursively sort elements before 
+				// partition and after partition 
+				sort(arr, low, pi-1); 
+				sort(arr, pi+1, high); 
+			} 
+		}  
+
 		//Metodo Radix, es el que implementa algoritmo de ordenamiento Radix sort
 		//Parametro: lista_ordenar es una estructura de datos con implementacion de objetos con interfaz Comparable, es la lista que se ordenara
 		//Return: lista_ordenada es una lista 
@@ -236,10 +300,9 @@ public class Main{
 	public static void main(String args[]) 
 	{
 		Integer[] orden = {2,4,3,1};
-		int[] orden2 = {2,4,3,1,10,9,7,8,6};
-		SortM.radix(orden2);
-		
-		
+		Integer[] orden2 = {2,4,3,1,10,9,7,8,6,100,99,1,3};
+		System.out.print(SortM.<Integer>merge(orden2));
+				
 	}
 
 }
